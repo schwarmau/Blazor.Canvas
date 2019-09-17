6cbc559594da80e235c93f302a79b3929b2d45e3
Austin Schwarm <austin.schwarm@uconn.edu>
2019-09-17 11:09:38 -0400

    Update Canvas2DContext.cs
    Finished adding non-experimental canvas methods

--- Blazor.Client.Canvas/Canvas2DContext.cs
+++ Blazor.Client.Canvas/Canvas2DContext.cs
@@ -18,37 +18,37 @@ namespace Blazor.Client.Canvas
         }
 
         #region Properties
-        public Task SetFillStyle(string value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.FillStyle, new[] { value });
-        public Task SetFont(string value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.Font, new[] { value });
-        public Task SetGlobalAlpha(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.GlobalAlpha, new[] { value });
-        public Task SetGlobalCompositeOperation(CompositionType value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.GlobalCompositeOperation, new[] { value });
-        public Task SetImageSmoothingEnabled(bool value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ImageSmoothingEnabled, new[] { value });
-        public Task SetLineCap(LineCap value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.LineCap, new[] { value });
-        public Task SetLineDashOffset(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.LineDashOffset, new[] { value });
-        public Task SetLineJoin(LineJoin value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.LineJoin, new[] { value });
-        public Task SetLineWidth(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.LineWidth, new[] { value });
-        public Task SetMiterLimit(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.MiterLimit, new[] { value });
-        public Task SetShadowBlur(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ShadowBlur, new[] { value });
-        public Task SetShadowColor(string value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ShadowColor, new[] { value });
-        public Task SetShadowOffsetX(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ShadowOffsetX, new[] { value });
-        public Task SetShadowOffsetY(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ShadowOffsetY, new[] { value });
-        public Task SetStrokeStyle(string value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.StrokeStyle, new[] { value });
-        public Task SetTextAlign(TextAlign value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.TextAlign, new[] { value });
-        public Task SetTextBaseline(TextBaseline value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.TextBaseline, new[] { value });
-        #endregion
+        public Task SetFillStyle(string value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.FillStyle, value);
+        public Task SetFont(string value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.Font, value);
+        public Task SetGlobalAlpha(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.GlobalAlpha, value);
+        public Task SetGlobalCompositeOperation(CompositionType value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.GlobalCompositeOperation, value);
+        public Task SetImageSmoothingEnabled(bool value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ImageSmoothingEnabled, value);
+        public Task SetLineCap(LineCap value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.LineCap, value);
+        public Task SetLineDashOffset(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.LineDashOffset, value);
+        public Task SetLineJoin(LineJoin value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.LineJoin, value);
+        public Task SetLineWidth(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.LineWidth, value);
+        public Task SetMiterLimit(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.MiterLimit, value);
+        public Task SetShadowBlur(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ShadowBlur, value);
+        public Task SetShadowColor(string value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ShadowColor, value);
+        public Task SetShadowOffsetX(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ShadowOffsetX, value);
+        public Task SetShadowOffsetY(float value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.ShadowOffsetY, value);
+        public Task SetStrokeStyle(string value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.StrokeStyle, value);
+        public Task SetTextAlign(TextAlign value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.TextAlign, value);
+        public Task SetTextBaseline(TextBaseline value) => _jsRuntime.InvokeAsync<Task>("canvasOperator.setCanvasProperty", Canvas, ContextProperties.TextBaseline, value);
+		#endregion
 
-        #region Methods
-        // TODO: implementation of methods (and contextmethods constants)
-        public Task Arc(float x, float y, float radius, float startAngle, float endAngle, bool antiClockwise = false);
+		#region Methods
+		// TODO: implementation of methods (and contextmethods constants)
+		public Task Arc(float x, float y, float radius, float startAngle, float endAngle, bool antiClockwise = false) => _jsRuntime.InvokeAsync<Task>("canvasOperator.callCanvasMethod", Canvas, ContextMethods.Arc, new object[] { x, y, radius, startAngle, endAngle, antiClockwise });
         public Task ArcTo(float x1, float y1, float x2, float y2, float radius);
         public Task BeginPath();
         public Task BezierCurveTo(float cp1x, float cp1y, float cp2x, float cp2y, float x, float y);
         public Task ClearRect(float x, float y, float width, float height);
         public Task Clip();
-        //public Task Clip(Path2D path = null); // TODO: implementation will require creating a C# wrapper for Path2D objects
-        public Task Clip(FillRule fillRule);
-        //public Task Clip(FillRule fillRule, Path2D path = null); // TODO: implementation will require creating a C# wrapper for Path2D objects
-        public Task ClosePath();
+        //public Task Clip(Path2D path); // TODO: implementation will require creating a C# wrapper for Path2D objects
+        public Task Clip(FillRule fillRule = FillRule.NonZero);
+		//public Task Clip(Path2D path, FillRule fillRule = FillRule.NonZero); // TODO: implementation will require creating a C# wrapper for Path2D objects
+		public Task ClosePath();
         //public Task<ImageData> CreateImageData(float width, float height); // TODO: implementation will require a C# wrapper for ImageData objects
         //public Task<ImageData> CreateImageData(ImageData imagedata); // TODO: implementation will require a C# wrapper for ImageData objects
         //public Task<CanvasGradient> CreateLinearGradient(float x0, float y0, float x1, float y1); // TODO: implementation will require a C# wrapper for CanvasGradient objects
@@ -60,8 +60,40 @@ namespace Blazor.Client.Canvas
         //public Task DrawImage(CanvasImageSource image, float dx, float dy, float dWidth, float dHeight); // TODO: implementation will require a C# wrapper for all variations of CanvasImageSource objects
         //public Task DrawImage(CanvasImageSource image, float sx, float sy, float sWidth, float sHeight, float dx, float dy, float dWidth, float dHeight); // TODO: implementation will require a C# wrapper for all variations of CanvasImageSource objects
         public Task Ellipse(float x, float y, float radiusX, float radiusY, float rotation, float startAngle, float endAngle, bool anticlockwise = false);
-        // TODO: remaining methods
-        #endregion
+		public Task Fill(FillRule fillRule = FillRule.NonZero);
+		//public Task Fill(Path2D path, FillRule fillRule = FillRule.NonZero); // TODO: implementation will require creating a C# wrapper for Path2D objects
+		public Task FillRect(float x, float y, float width, float height);
+		public Task FillText(string text, float x, float y);
+		public Task FillText(string text, float x, float y, int maxWidth);
+		//public Task<ImageData> GetImageData(float sx, float sy, float sw, float sh); // TODO: implementation will require a C# wrapper for ImageData objects
+		public Task<float[]> GetLineDash();
+		public Task<bool> IsPointInPath(float x, float y, FillRule fillRule = FillRule.NonZero);
+		//public Task<bool> IsPointInPath(Path2D path, float x, float y, FillRule fillRule = FillRule.NonZero); // TODO: implementation will require creating a C# wrapper for Path2D objects
+		public Task<bool> IsPointInStroke(float x, float y);
+		//public Task<bool> IsPointInStroke(Path2D path, float x, float y); // TODO: implementation will require creating a C# wrapper for Path2D objects
+		public Task LineTo(float x, float y);
+		//public Task<TextMetrics> MeasureText(string text); // TODO: implementation will require creating a C# wrapper for TextMetrics objects
+        public Task MoveTo(float x, float y);
+		//public Task PutImageData(ImageData imageData, float dx, float dy); // TODO: implementation will require a C# wrapper for ImageData objects
+		//public Task PutImageData(ImageData imageData, float dx, float dy, float dirtyX, float dirtyY, float dirtyWidth, float dirtyHeight); // TODO: implementation will require a C# wrapper for ImageData objects
+		public Task QuadraticCurveTo(float cpx, float cpy, float x, float y);
+		public Task Rect(float x, float y, float width, float height);
+		public Task Restore();
+		public Task Rotate(double angle);
+		public Task Save();
+		public Task Scale(double x, double y);
+		public Task SetLineDash(float[] segments);
+		public Task SetTransform(double a, double b, double c, double d, float e, float f);
+		//public Task SetTransform(DOMMatrixInit matrix); // TODO: implementation will require a C# wrapper for DOMMatrixInit objects
+		public Task Stroke();
+		//public Task Stroke(Path2D path); // TODO: implementation will require a C# wrapper for Path2D objects
+		public Task StrokeRect(float x, float y, float width, float height);
+		public Task StrokeText(string text, float x, float y);
+		public Task StrokeText(string text, float x, float y, int maxWidth);
+		public Task Transform(double a, double b, double c, double d, float e, float f);
+		//public Task Transform(DOMMatrixInit matrix); // TODO: implementation will require a C# wrapper for DOMMatrixInit objects
+		public Task Translate(float x, float y);
+		#endregion
 
         #region Batching Support
         public async Task ExecuteBatch(Canvas2DBatch batch)
